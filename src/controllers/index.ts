35e39af6c056eef6e7d016f642cbb889dc21a336
anovazzi1 <otavio2204@gmail.com>
2023-09-21 18:37:27 -0300

    chore(index.html): update Langflow Chat component attributes to match new flow ID, chat input field name, and API key fix(index.ts): update headers object to include "api-key" header when provided in sendMessage function

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -9,9 +9,10 @@ export async function sendMessage(baseUrl: string, flowId: string, message: stri
     else {
         data = { inputs: inputs };
     }
-    if(api_key){
-        data =  {...data, api_key};
+    const headers:{[key:string]:string}= {"Content-Type": "application/json"}
+    if( api_key){
+        headers["api-key"]=api_key;
     }
-    let response = axios.post(`${baseUrl}/api/v1/process/${flowId}`, data,{headers:{"Content-Type": "application/json"}});
+    let response = axios.post(`${baseUrl}/api/v1/process/${flowId}`, data,{headers});
     return response;
 }
